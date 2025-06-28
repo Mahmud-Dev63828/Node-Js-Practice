0b6bffd32fc60ec8ef339c39d169b14a87be5261
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-06-28 23:24:25 +0600

    create  folder learned

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const terminalInput = process.argv;
 const path = require('path')
 const fs = require('fs')
+const {log} = require('console')
 const [, , command, folderName, fileName, fileValue] = terminalInput
 
 console.log(command,folderName,fileName,fileValue);
 
 switch (command) {
-    case 'addFile':{
+    case 'addFolder':{
+      const   isExistFolder = fs.existsSync('node')
+      if (isExistFolder){
+        log('this folder is already exist . try another')
+      } else {
 
+          fs.mkdir('node', (err)=>{
+           if(err){
+               log('error to make directory',err)
+           }
+          })
+      }
     }
         
 
