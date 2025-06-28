b599190b6d8ddd33dd4f1421d40d0eb23a9042f3
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-06-28 19:31:27 +0600

    project set uped

---
+++ index.js
@@ -0,0 +1,14 @@
+const terminalInput = process.argv;
+const path = require('path')
+const fs = require('fs')
+const [, , command, folderName, fileName, fileValue] = terminalInput
+
+console.log(command,folderName,fileName,fileValue);
+
+switch (command) {
+    case 'addFile':{
+
+    }
+        
+
+}
